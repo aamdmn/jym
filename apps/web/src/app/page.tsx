8d235cbaa545e5be43f816751db0c172c721ddd2
Adam Demian <adamdemian1@gmail.com>
2025-09-18 15:23:38 +0200

    init

---
+++ apps/web/src/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-sans sm:p-20">
+      <main className="flex w-full max-w-4xl flex-col items-start justify-center">
+        <h1 className="mt-52 font-bold text-2xl">jym</h1>
+
+        <p className="mt-10 text-gray-500 text-xl">coming soon...</p>
+      </main>
+    </div>
+  );
+}
