5b74817172e44a501ea253a9825780b5744afddd
Adam Demian <adamdemian1@gmail.com>
2025-09-18 15:25:19 +0200

    triger

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
       <main className="flex w-full max-w-4xl flex-col items-start justify-center">
         <h1 className="mt-52 font-bold text-2xl">jym</h1>
 
-        <p className="mt-10 text-gray-500 text-xl">coming soon...</p>
+        <p className="mt-10 text-gray-500 text-xl">coming soon....</p>
       </main>
     </div>
   );
